b9bc4567d7c0b9fd8bec66ee3ba556f50c6fcfb7
Garrett Gitterman <garrettgitterman@garrettrmansMBP.home>
2014-11-09 21:09:47 -0500

    finished project

--- TableViewController.h
+++ TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Garrett Gitterman on 11/9/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
